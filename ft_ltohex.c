bbdd05622457d5bac3dfe9c2d326d450ccea550d
Sulaiman YUSOF <syusof@e3r5p1.42.fr>
2015-12-17 13:33:23 +0100

    r

--- ft_ltohex.c
+++ ft_ltohex.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 02:47:12 by syusof            #+#    #+#             */
-/*   Updated: 2015/12/17 12:44:32 by syusof           ###   ########.fr       */
+/*   Updated: 2015/12/17 13:32:11 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 char	*ft_ltohex(long l)
 {
 	char	*res;
+	char	*bigi;
 	int i;
 	long l1;
-	int tab[3];
+	int tab[100];
 
+	bigi = NULL;
 	l1 = l;
 	i = 1;
 	while(l > 16)
@@ -29,6 +31,7 @@ char	*ft_ltohex(long l)
 	}
 	res = (char*)malloc(sizeof(char)*(i + 1));
 	res[i] = 0;
+	bigi  = res;
 	l = l1;
 //	res = res + i;
 	while (l > 16)
@@ -48,7 +51,7 @@ char	*ft_ltohex(long l)
 		else if (l % 16 == 15)
 			*res = 'f';
 		l = l / 16;
-		printf("%s\n",res);
+//		printf("%s\n",res);
 		res++;
 	}
 		if (l % 16 < 10)
@@ -66,28 +69,25 @@ char	*ft_ltohex(long l)
 		else if (l % 16 == 15)
 			*res = 'f';
 		
-		printf("%s\n",res);
-	int j = 0;
+//		printf("%s\n",res);
+
+		int j = 0;
+		tab[j] = *res;
 		while (j < i-1)
 		{
-			tab[j] = *res;
 			res--;
 			j++;
+			tab[j] = *res;
 		}
 		j = 0;
-		res++;
+		*res = tab[j];
 		while (j < i-1)
 		{
-			*res = tab[j];
 			res++;
 			j++;
+			*res = tab[j];
 		}
 		j = 0;
-		while (j < i)
-		{
-			j++;
-			res--;
-		}
-		printf("der = %s\n",res);
-	return (res);
+//		printf("der = %s\n",res);
+	return (bigi);
 }
