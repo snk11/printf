33a93a20631d764d67eaebf38fb3a945c71d09f3
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-03-22 04:12:17 +0100

    r

---
+++ ft_putlongnbr.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putlongnbr.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/16 01:40:19 by syusof            #+#    #+#             */
+/*   Updated: 2016/01/26 11:51:30 by syusof           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ft_printf.h"
+
+void	ft_putlongnbr(long n)
+{
+	if (n == LONG_MIN)
+		ft_putstr("-9223372036854775808");
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n = -n;
+		}
+		if (n >= 10)
+		{
+			ft_putlongnbr(n / 10);
+			ft_putlongnbr(n % 10);
+		}
+		else
+			ft_putchar(n + '0');
+	}
+}
+
+void	ft_putulongnbr(unsigned long n)
+{
+	unsigned long	c;
+
+	c = 0;
+	if (n == ULONG_MAX)
+	{
+		ft_putstr("18446744073709551615");
+	}
+	else
+	{
+		if (n >= 10)
+		{
+			ft_putlongnbr(n / 10);
+			ft_putlongnbr(n % 10);
+		}
+		else
+			ft_putchar(n + '0');
+	}
+}
