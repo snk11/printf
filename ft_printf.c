a0ac2e81d91e8b8461eadd0e66441ffcda0be43e
Sulaiman YUSOF <syusof@e3r7p2.42.fr>
2016-04-06 15:16:40 +0200

    r

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 01:53:43 by syusof            #+#    #+#             */
-/*   Updated: 2016/04/06 15:08:35 by syusof           ###   ########.fr       */
+/*   Updated: 2016/04/06 15:15:57 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_printf.h"
 
 # include <stdio.h>
-//+3.3-06 d
+
 int	ft_printf(char *str, ...)
 {
 	va_list		ap;
