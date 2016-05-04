d88ced2672af7e5f5ee14348aecb2a3e190d84ad
Sulaiman YUSOF <syusof@e3r8p2.42.fr>
2016-05-04 13:52:08 +0200

    r

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,21 +6,17 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 16:55:06 by syusof            #+#    #+#             */
-/*   Updated: 2016/04/26 17:21:43 by syusof           ###   ########.fr       */
+/*   Updated: 2016/05/04 13:51:03 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr(int n,t_numb *e)
+int	ft_putnbr(int n, t_numb *e)
 {
-	int		i;
-	int		j;
 	int		r1;
 
-	i = 0;
 	r1 = 0;
-	i = ft_count(n);
 	if (e->valzero == 0)
 	{
 		if (n == -2147483648)
@@ -31,7 +27,7 @@ int	ft_putnbr(int n,t_numb *e)
 			}
 		}
 		else
-			r1 = r1 + ft_putnbr1(n,e);
+			r1 = r1 + ft_putnbr1(n, e);
 	}
 	else if (e->valzero == 1)
 		r1 = r1 + ft_putnbr42(e);
@@ -51,7 +47,7 @@ int	ft_putnbr42(t_numb *e)
 		{
 			if (e->indplus == 1)
 				e->w--;
-			if (e->indspace== 1)
+			if (e->indspace == 1)
 				e->w--;
 			r1 = r1 + ft_putnbr42b(e);
 		}
@@ -71,7 +67,7 @@ int	ft_putnbr42a(t_numb *e)
 	r1 = 0;
 	if (e->indplus == 1)
 		e->w--;
-	while(e->w - e->pr > 0)
+	while (e->w - e->pr > 0)
 	{
 		ft_putchar(' ');
 		r1++;
@@ -82,7 +78,7 @@ int	ft_putnbr42a(t_numb *e)
 		ft_putchar('+');
 		r1++;
 	}
-	while((e->pr) > 0)
+	while ((e->pr) > 0)
 	{
 		ft_putchar('0');
 		r1++;
@@ -120,16 +116,15 @@ int	ft_putnbr42b(t_numb *e)
 	return (r1);
 }
 
-int	ft_putnbr1(int n,t_numb *e)
+int	ft_putnbr1(int n, t_numb *e)
 {
 	int		i;
-	int		j;
 	int		r1;
 
 	i = 0;
 	r1 = 0;
 	i = ft_count(n);
-	if (n < 0 )
+	if (n < 0)
 	{
 		if (e->indzero == 1 && e->w > i && e->pr == 0 && e->indminus == 0)
 		{
@@ -138,8 +133,8 @@ int	ft_putnbr1(int n,t_numb *e)
 	}
 	if (n >= 10)
 	{
-		r1 = r1 + ft_putnbr(n / 10,e);
-		r1 = r1 + ft_putnbr(n % 10,e);
+		r1 = r1 + ft_putnbr(n / 10, e);
+		r1 = r1 + ft_putnbr(n % 10, e);
 	}
 	else
 	{
@@ -149,7 +144,7 @@ int	ft_putnbr1(int n,t_numb *e)
 	return (r1);
 }
 
-int	ft_putnbr2(int n,t_numb *e)
+int	ft_putnbr2(int n, t_numb *e)
 {
 	int		i;
 	int		j;
@@ -166,28 +161,24 @@ int	ft_putnbr2(int n,t_numb *e)
 		}
 	}
 	else
-		r1 = r1 + ft_putnbr2a(n,e);
+		r1 = r1 + ft_putnbr2a(n, e);
 	return (r1);
 }
 
-int	ft_putnbr2a(int n,t_numb *e)
+int	ft_putnbr2a(int n, t_numb *e)
 {
-	int		i;
-	int		j;
 	int		r1;
 
-	i = 0;
 	r1 = 0;
-	i = ft_count(n);
-	if (n < 0 )
+	if (n < 0)
 	{
-		r1 = r1 + ft_putnbr2a1(n,e);
+		r1 = r1 + ft_putnbr2a1(n, e);
 		n = -n;
 	}
 	if (n >= 10)
 	{
-		r1 = r1 + ft_putnbr2(n / 10,e);
-		r1 = r1 + ft_putnbr2(n % 10,e);
+		r1 = r1 + ft_putnbr2(n / 10, e);
+		r1 = r1 + ft_putnbr2(n % 10, e);
 	}
 	else
 	{
@@ -197,10 +188,9 @@ int	ft_putnbr2a(int n,t_numb *e)
 	return (r1);
 }
 
-int	ft_putnbr2a1(int n,t_numb *e)
+int	ft_putnbr2a1(int n, t_numb *e)
 {
 	int		i;
-	int		j;
 	int		r1;
 
 	i = 0;
@@ -216,4 +206,3 @@ int	ft_putnbr2a1(int n,t_numb *e)
 	}
 	return (r1);
 }
-
