90fb7108ae8739b6d4bb0de526b8598ea2301ed3
Sulaiman YUSOF <syusof@e3r8p2.42.fr>
2016-05-04 13:24:00 +0200

    r

--- ft_ltooct.c
+++ ft_ltooct.c
@@ -6,21 +6,21 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 14:11:15 by syusof            #+#    #+#             */
-/*   Updated: 2016/03/23 01:45:06 by syusof           ###   ########.fr       */
+/*   Updated: 2016/05/04 13:23:48 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_printf.h"
+#include "ft_printf.h"
 
 char	*ft_ltooct(long l)
 {
 	char	*res;
-	int i;
-	long l1;
+	int		i;
+	long	l1;
 
 	l1 = l;
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
@@ -28,25 +28,25 @@ char	*ft_ltooct(long l)
 	if (l1 >= 512 && l1 <= 575)
 	{
 		i++;
-		res = (char*)malloc(sizeof(char)*(i + 1));
+		res = (char*)malloc(sizeof(char) * (i + 1));
 	}
 	else
-		res = (char*)malloc(sizeof(char)*(i + 1));
+		res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
 	l = l1;
 	if (l1 >= 512 && l1 <= 575)
-		ft_ltooct10(res,l,i);
+		ft_ltooct10(res, l, i);
 	else
-		ft_ltooct11(res,l,i);
+		ft_ltooct11(res, l, i);
 	return (res);
 }
 
-void	ft_ltooct10(char *res,long l,int i)
+void	ft_ltooct10(char *res, long l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
@@ -55,193 +55,191 @@ void	ft_ltooct10(char *res,long l,int i)
 	*res = '1';
 }
 
-void	ft_ltooct11(char *res,long l,int i)
+void	ft_ltooct11(char *res, long l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
 
-void	ft_ltooct12(char *res,unsigned long l,int i)
+void	ft_ltooct12(char *res, unsigned long l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
 
-void	ft_ltooct13(char *res,unsigned short l,int i)
+void	ft_ltooct13(char *res, unsigned short l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
 
-void	ft_ltooct14(char *res,unsigned long long l,int i)
+void	ft_ltooct14(char *res, unsigned long long l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
 
-void	ft_ltooct15(char *res,unsigned short int l,int i)
+void	ft_ltooct15(char *res, unsigned short int l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
 
-void	ft_ltooct16(char *res,unsigned char l,int i)
+void	ft_ltooct16(char *res, unsigned char l, int i)
 {
 	res = res + i - 1;
 	while (l > 8)
 	{
-			*res = '0' + l % 8;
+		*res = '0' + l % 8;
 		l = l / 8;
 		res--;
 	}
 	*res = '0' + l % 8;
 }
-
 
 char	*ft_ltooct2(unsigned long l)
 {
-	char	*res;
-	char	*bigi;
-	int i;
-	 unsigned long l1;
+	char			*res;
+	char			*bigi;
+	int				i;
+	unsigned long	l1;
 
 	bigi = NULL;
 	l1 = l;
 	if (l == LONG_MIN)
 		return ("1000000000000000000000");
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
 	}
-	res = (char*)malloc(sizeof(char)*(i + 1));
+	res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
-	bigi  = res;
+	bigi = res;
 	l = l1;
-	ft_ltooct12(res,l,i);
+	ft_ltooct12(res, l, i);
 	return (bigi);
 }
 
 char	*ft_ltooct3(unsigned short l)
 {
-	char	*res;
-	char	*bigi;
-	int i;
-	 unsigned short l1;
+	char			*res;
+	char			*bigi;
+	int				i;
+	unsigned short	l1;
 
 	bigi = NULL;
 	l1 = l;
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
 	}
-	res = (char*)malloc(sizeof(char)*(i + 1));
+	res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
-	bigi  = res;
+	bigi = res;
 	l = l1;
-	ft_ltooct13(res,l,i);
+	ft_ltooct13(res, l, i);
 	return (bigi);
 }
 
 char	*ft_ltooct4(unsigned long long l)
 {
-	char	*res;
-	char	*bigi;
-	int i;
-	 unsigned long long l1;
+	char				*res;
+	char				*bigi;
+	int					i;
+	unsigned long long	l1;
 
 	bigi = NULL;
 	l1 = l;
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
 	}
-	res = (char*)malloc(sizeof(char)*(i + 1));
+	res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
-	bigi  = res;
+	bigi = res;
 	l = l1;
-	ft_ltooct14(res,l,i);
+	ft_ltooct14(res, l, i);
 	return (bigi);
 }
 
 char	*ft_ltooct5(unsigned short int l)
 {
-	char	*res;
-	char	*bigi;
-	int i;
-	 unsigned short int l1;
+	char				*res;
+	char				*bigi;
+	int					i;
+	unsigned short int	l1;
 
 	bigi = NULL;
 	l1 = l;
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
 	}
-	res = (char*)malloc(sizeof(char)*(i + 1));
+	res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
-	bigi  = res;
+	bigi = res;
 	l = l1;
-	ft_ltooct15(res,l,i);
+	ft_ltooct15(res, l, i);
 	return (bigi);
 }
 
 char	*ft_ltooct6(unsigned char l)
 {
-	char	*res;
-	char	*bigi;
-	int i;
-	 unsigned char l1;
+	char			*res;
+	char			*bigi;
+	int				i;
+	unsigned char	l1;
 
 	bigi = NULL;
 	l1 = l;
 	i = 1;
-	while(l > 8)
+	while (l > 8)
 	{
 		l = l / 8;
 		i++;
 	}
-	res = (char*)malloc(sizeof(char)*(i + 1));
+	res = (char*)malloc(sizeof(char) * (i + 1));
 	res[i] = 0;
-	bigi  = res;
+	bigi = res;
 	l = l1;
-	ft_ltooct16(res,l,i);
+	ft_ltooct16(res, l, i);
 	return (bigi);
 }
-
