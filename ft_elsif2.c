71a4c2d22cfb40082047bac25808b46fea4bfe33
Sulaiman YUSOF <syusof@e3r8p2.42.fr>
2016-05-04 13:07:36 +0200

    r

--- ft_elsif2.c
+++ ft_elsif2.c
@@ -6,31 +6,30 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 14:21:18 by syusof            #+#    #+#             */
-/*   Updated: 2016/04/18 11:22:22 by syusof           ###   ########.fr       */
+/*   Updated: 2016/05/04 13:03:13 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
-# include "ft_printf.h"
-
-int		ft_elsif2(char ***str,t_numb *e,va_list ap)
+int		ft_elsif2(char ***str, t_numb *e, va_list ap)
 {
 	int	cnt;
 
 	cnt = 0;
 	if (e->indelsif == 0)
-		cnt = ft_case8(&str,e,ap);
+		cnt = ft_case8(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case9(&str,e,ap);
+		cnt = ft_case9(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case10(&str,e,ap);
+		cnt = ft_case10(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case11(&str,e,ap);
+		cnt = ft_case11(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case12(&str,e,ap);
+		cnt = ft_case12(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case13(&str,e,ap);
+		cnt = ft_case13(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case14(&str,e,ap);
+		cnt = ft_case14(&str, e, ap);
 	return (cnt);
 }
--- ft_elsif3.c
+++ ft_elsif3.c
@@ -6,26 +6,24 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 14:22:33 by syusof            #+#    #+#             */
-/*   Updated: 2016/04/18 11:22:29 by syusof           ###   ########.fr       */
+/*   Updated: 2016/05/04 13:03:41 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
-
-# include "ft_printf.h"
-
-int		ft_elsif3(char ***str,t_numb *e,va_list ap)
+int		ft_elsif3(char ***str, t_numb *e, va_list ap)
 {
 	int	cnt;
 
 	cnt = 0;
 	if (e->indelsif == 0)
-		cnt = ft_case15(&str,e,ap);
+		cnt = ft_case15(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case16(&str,e,ap);
+		cnt = ft_case16(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case17(&str,e,ap);
+		cnt = ft_case17(&str, e, ap);
 	if (e->indelsif == 0)
-		cnt = ft_case18(&str,e,ap);
+		cnt = ft_case18(&str, e, ap);
 	return (cnt);
 }
